f284fc9629094cffe6b97580af7d4870f20b1394
Youngseo Yoo <7dudtj@naver.com>
2021-01-11 23:15:36 +0900

    1271_엄청난 부자2

---
+++ Math/1271_엄청난 부자2/1271_엄청난 부자2.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    BigInteger a = sc.nextBigInteger();
+    BigInteger b = sc.nextBigInteger();
+
+    System.out.println(a.divide(b));
+    System.out.println(a.mod(b));
+  }
+}
