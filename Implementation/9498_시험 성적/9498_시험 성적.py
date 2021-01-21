2153edebae6f95dc0d750c449c5322f343c9f980
Youngseo Yoo <7dudtj@naver.com>
2021-01-21 23:32:08 +0900

    9498_시험 성적

---
+++ Implementation/9498_시험 성적/9498_시험 성적.py
@@ -0,0 +1,11 @@
+N = int(input())
+if 90 <= N <= 100:
+    print('A')
+elif 80 <= N < 90:
+    print('B')
+elif 70 <= N < 80:
+    print('C')
+elif 60 <= N < 70:
+    print('D')
+else:
+    print('F')
