ae56b65e90e46a6f84e95e2f5e366c26d380931a
Youngseo Yoo <7dudtj@naver.com>
2021-01-31 15:44:20 +0900

    15680_연세대학교

---
+++ Implementation/15680_연세대학교/15680_연세대학교.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    
+    cin >> N;
+    
+    if (N == 0)
+        cout << "YONSEI" << endl;
+    else
+        cout << "Leading the Way to the Future" << endl;
+}
