008f8306be7f988bb3454af5db1fc8a83e18214c
7dudtj <7dudtj@naver.com>
2023-02-12 16:52:04 +0900

    27210_신을 모시는 사당.java

---
+++ DP/27210_신을 모시는 사당/27210_신을 모시는 사당.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+
+	public static void main(String[] args) throws IOException{
+
+		// set needed variables
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] stone = new int[N];
+
+		// get stones
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++){
+			stone[i] = Integer.parseInt(st.nextToken());
+		}
+
+		// count answer
+		int max = 0;
+		int min = 0;
+		int count = 0;
+
+		for (int i = 0; i < N; i++){
+			if (stone[i] == 1) count++;
+			else count--;
+			if (count > max) max = count;
+			if (count < min) min = count;
+		}
+		int answer = Math.abs(max - min);
+
+		// print answer and end program
+		System.out.print(answer);
+		br.close();
+		return;
+	}
+}
