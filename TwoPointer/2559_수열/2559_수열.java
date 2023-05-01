2f747d6c3255d322a5fb9b55efc0f1bdd04629d6
7dudtj <7dudtj@naver.com>
2023-05-01 21:40:49 +0900

    2559_수열.java

---
+++ TwoPointer/2559_수열/2559_수열.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		// set needed variables
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] inputs = br.readLine().split(" ");
+    int N = Integer.parseInt(inputs[0]);
+    int K = Integer.parseInt(inputs[1]);
+    int[] temps = new int[N];
+    inputs = br.readLine().split(" ");
+    for (int i = 0; i < N; i++){
+        temps[i] = Integer.parseInt(inputs[i]);
+    }
+    int answer = Integer.MIN_VALUE;
+    int window = 0;
+
+    // find first continuous value
+    for (int i = 0; i < K; i++){
+        window += temps[i];
+    }
+    answer = window;
+
+    // find maximum window
+    for (int i = 1; i <= N - K; i++){
+      // slide window
+        window -= temps[i - 1];
+        window += temps[i + K - 1];
+
+      // find big window
+        if (window > answer){
+        answer = window;
+        }
+    }
+
+    // print answer and end program
+    System.out.print(answer);
+    br.close();
+    return;
+	}
+}
