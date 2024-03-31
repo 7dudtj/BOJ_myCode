46d5eaf1319e7173a2751f9f527f73016af88edb
7dudtj <7dudtj@naver.com>
2024-03-31 17:19:11 +0900

    25083_새싹.c

---
+++ Implementation/25083_새싹/25083_새싹.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+}
