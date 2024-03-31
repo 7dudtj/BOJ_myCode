5beae6816b5ed7a1b3246b824473abcf1b763990
7dudtj <7dudtj@naver.com>
2024-03-31 17:16:05 +0900

    10172_개.c

---
+++ Implementation/10172_개/10172_개.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|\n");
+}
