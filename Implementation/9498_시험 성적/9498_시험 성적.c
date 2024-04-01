8050a5c4cd6cbf6e194906a98d36a01fae194123
7dudtj <7dudtj@naver.com>
2024-04-01 20:37:16 +0900

    9498_시험 성적.c

---
+++ Implementation/9498_시험 성적/9498_시험 성적.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void){
+    int a;
+    scanf("%d", &a);
+    if (a >= 90)
+    {
+        printf("A");
+    }
+    else if (a >= 80)
+    {
+        printf("B");
+    }
+    else if (a >= 70)
+    {
+        printf("C");
+    }
+    else if (a >= 60)
+    {
+        printf("D");
+    }
+    else
+    {
+        printf("F");
+    }
+}
