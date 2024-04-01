f5cd24410c71cfc8a5e7316b8dd7dd753772ab7e
7dudtj <7dudtj@naver.com>
2024-04-01 20:30:53 +0900

    1330_두 수 비교하기.c

---
+++ Implementation/1330_두 수 비교하기/1330_두 수 비교하기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    if (a > b)
+    {
+        printf(">");
+    }
+    else if (a < b){
+        printf("<");
+    }
+    else{
+        printf("==");
+    }
+}
